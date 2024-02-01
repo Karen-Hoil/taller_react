8f6521119b60b61c39790aeec1a389c05898bf22
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2024-01-31 19:43:40 -0600

    Login funcional

--- src/page/login.js
+++ src/page/login.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState} from 'react';
+import axios from "axios";
 import '../css/login.css'; 
 import icono_herramienta from "../img/icono_herramienta.png";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async (e) => {
+    if (!email || !password ) {
+      alert("Todos los campos deben ser completados");
+      return;
+    }
+
+    e.preventDefault();
+    const response = await axios.post("http://localhost:8082/login", {
+      correo: email,
+      contraseña: password,
+    });
+    if (response.data.status) {
+      window.location.href = "/inicio";
+    } else {
+      setEmail("");
+      setPassword("");
+      alert("Prueba con otro correo o contraseña");
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-header">
         <h1>Taller dashboard</h1>
         <img src={icono_herramienta} alt="..." />
       </div>
-      <form className="login-form">
+      <form className="login-form" onSubmit={login}>
         <label htmlFor="email">Correo:</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
         <label htmlFor="password">Contraseña:</label>
-        <input type="password" id="password" name="password" />
+        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-        <a href='/inicio'><button type="submit" className="login-button">Iniciar sesión</button></a>
+        <button type="submit" className="login-button">Iniciar sesión</button>
       </form>
       <div className="register-link">
         <p>¿Eres nuevo? <a href="/registro">Regístrate</a></p> 
@@ -25,4 +50,3 @@ const Login = () => {
 };
 
 export default Login;
-
